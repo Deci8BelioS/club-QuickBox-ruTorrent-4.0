b248f3820cd6482d8f8976ef47db6f38a825c6fc
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2024-05-02 17:31:19 +0200

    Create es.js

---
+++ lang/es.js
@@ -0,0 +1,12 @@
+/*
+ * TEMA DEL PLUGIN
+ *
+ * Archivo en Español.
+ *
+ * Autor: JMSolo - https://plaza.quickbox.io/
+ */
+
+ theUILang.themeStandard	= "QuickBox";
+ theUILang.theme		= "Tema";
+
+thePlugins.get("Tema").langLoaded();
